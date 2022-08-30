0d74b707c228cdcfb0c786532094047cd6a6833c
Chris Pennington <chris@codinginpublic.dev>
2022-08-30 06:01:19 -0600

    lesson-2 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 
+// custom components
+import CustomForm from './components/CustomForm'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const addTask = (task) => {
+    console.log(task)
+  }
 
   return (
-    <div className="App">
-
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      <CustomForm addTask={addTask}/>
     </div>
   )
 }
---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+// library imports
+import { PlusIcon } from '@heroicons/react/24/solid'
+
+const CustomForm = ({ addTask }) => {
+  const [task, setTask] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addTask({
+      name: task,
+      checked: false,
+      id: Date.now()
+    })
+    setTask("")
+  }
+
+  return (
+    <form
+      className="todo"
+      onSubmit={handleFormSubmit}
+      >
+      <div className="wrapper">
+        <input
+          type="text"
+          id="task"
+          className="input"
+          value={task}
+          onInput={(e) => setTask(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label
+          htmlFor="task"
+          className="label"
+        >Enter Task</label>
+      </div>
+      <button
+        className="btn"
+        aria-label="Add Task"
+        type="submit"
+        >
+        <PlusIcon />
+      </button>
+    </form>
+  )
+}
+export default CustomForm
